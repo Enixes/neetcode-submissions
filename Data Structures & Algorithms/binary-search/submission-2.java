0909ce8dd42127262abbc93418ab03aeda447c41
Asu Singh <enixes7@gmail.com>
2026-06-30 00:19:38 +0530

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.java
@@ -0,0 +1,21 @@
+class Solution {
+	public int search(int[] nums, int target) {
+		int start = 0, end = nums.length - 1;
+
+		int mid = (end + start) / 2;
+
+		while (start <= end) {
+			if (nums[mid] == target) {
+				return mid;
+			} else if (nums[mid] > target) {
+				end = mid - 1;
+			} else {
+				start = mid + 1;
+			}
+			mid = (start + end)/2;
+
+		}
+
+		return -1;
+	}
+}
